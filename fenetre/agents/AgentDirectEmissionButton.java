8616b513239867f227b7637ea64b0023a7627596
EmmanuelADAM <32464906+EmmanuelADAM@users.noreply.github.com>
2019-07-01 00:01:40 +0200

    Create AgentDirectEmissionButton.java

---
+++ fenetre/agents/AgentDirectEmissionButton.java
@@ -0,0 +1,46 @@
+package fenetre.agents;
+
+import fenetre.gui.SimpleWindow4Agent;
+import jade.core.AID;
+import jade.gui.GuiEvent;
+import jade.lang.acl.ACLMessage;
+
+
+/**
+ * agents associé à une fenêtre, envoie un message direct aux agents b,c,d lorsque la fenêtre lui informe que le bouton a été cliqué
+ * @author eadam
+ */
+@SuppressWarnings("serial")
+public class AgentDirectEmissionButton extends AgentWindowed{
+
+	/**
+	 * initialize the agents <br>
+	 * create the local dir to store data and roles <br>
+	 * add the stack of behaviours (pileComportements)
+	 */
+	protected void setup() {  
+		window = new SimpleWindow4Agent(getAID().getName(), this);
+		println("Hello! Agent  " +  getLocalName() + " is ready, my address is " + this.getAID().getName());
+		window.setButtonActivated(true);
+	}
+	
+	  /**reaction to a gui event*/
+	  protected void onGuiEvent(GuiEvent ev) {
+	    switch(ev.getType()) {
+	      case -1:  sendMessages(); break;
+	    }
+	  }
+
+	  /**send messages to agents b, c & d*/
+	private void sendMessages()
+	{
+		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+		msg.setContent("salut");
+		msg.addReceiver(new AID("b", false));
+		msg.addReceiver(new AID("c", false));
+		msg.addReceiver(new AID("d", false));
+		send(msg);
+	}
+
+
+}
