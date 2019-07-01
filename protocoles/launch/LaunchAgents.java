783db12930c62f87f9f3038e7ccc603c21c9a0b3
EmmanuelADAM <32464906+EmmanuelADAM@users.noreply.github.com>
2019-07-01 10:53:27 +0200

    Create LaunchAgents.java

---
+++ protocoles/launch/LaunchAgents.java
@@ -0,0 +1,23 @@
+package protocoles.launch;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.util.ExtendedProperties;
+import jade.util.leap.Properties;
+
+public class LaunchAgents {
+    public static void main(String[] args) {
+        // preparer les arguments pout le conteneur JADE
+        Properties prop = new ExtendedProperties();
+        // demander la fenetre de controle
+        prop.setProperty(Profile.GUI, "true");
+        // nommer les agents
+        prop.setProperty(Profile.AGENTS, "a:protocoles.agents.AgentEmissionARE;b:protocoles.agents.AgentReceptionARE");
+        // creer le profile pour le conteneur principal
+        ProfileImpl profMain = new ProfileImpl(prop);
+        // lancer le conteneur principal
+        Runtime rt = Runtime.instance();
+        rt.createMainContainer(profMain);
+    }
+}
