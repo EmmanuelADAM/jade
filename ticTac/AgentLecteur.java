1f7e40a4d9474057b51722ad56cd4b35a66b18e0
EmmanuelADAM <32464906+EmmanuelADAM@users.noreply.github.com>
2019-11-29 02:36:11 +0100

    Create AgentLecteur.java

---
+++ ticTac/AgentLecteur.java
@@ -0,0 +1,56 @@
+package ticTac;
+
+import jade.core.Runtime;
+import jade.core.*;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.WakerBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.util.ExtendedProperties;
+import jade.util.leap.Properties;
+
+import static java.lang.System.out;
+
+/**
+ * classe d'agent qui ecoute 2 types de messages.
+ *
+ * @author emmanueladam
+ * */
+public class AgentLecteur extends Agent {
+    /**
+     * Initialisation de l'agents
+     */
+    @Override
+    protected void setup() {
+        out.println("De l'agent " + getLocalName());
+
+        // ajout d'un comportement qui attend des msgs de type clock
+        addBehaviour(new CyclicBehaviour(this) {
+            MessageTemplate mt = MessageTemplate.MatchConversationId("CLOCK");
+
+            public void action() {
+                var msg = receive(mt);
+                if (msg != null) {
+                    var content = msg.getContent();
+                    var sender = msg.getSender();
+                    out.println("agent " + getLocalName() + " : j'ai reçu " + content + " de " + sender);
+                } else block();
+            }
+        });
+        // ajout d'un comportement qui attend des msgs de type boom
+        addBehaviour(new CyclicBehaviour(this) {
+            MessageTemplate mt = MessageTemplate.MatchConversationId("BOOM");
+
+            public void action() {
+                var msg = receive(mt);
+                if (msg != null) {
+                    var content = msg.getContent();
+                    var sender = msg.getSender();
+                    out.println("attention !!!! moi agent  " + getLocalName() + " : j'ai reçu " + content + " de " + sender);
+                } else block();
+            }
+        });
+    }
+
+}
