86f200258e28890f757639f5af8be5ace7c159eb
EmmanuelADAM <32464906+EmmanuelADAM@users.noreply.github.com>
2019-11-29 02:34:32 +0100

    Create AgentPosteur.java

---
+++ ticTac/AgentPosteur.java
@@ -0,0 +1,54 @@
+package ticTac;
+
+import jade.core.Runtime;
+import jade.core.*;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.core.behaviours.WakerBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.util.ExtendedProperties;
+import jade.util.leap.Properties;
+
+import static java.lang.System.out;
+
+/**
+ * classe d'agents pour échange entre 1 agents de cette classe et 1 autre<br>
+ *     2 msg de type differents sont envoyes
+ * @author emmanueladam
+ * */
+public class AgentPosteur extends Agent {
+    /**
+     * Initialisation de l'agents
+     */
+    @Override
+    protected void setup() {
+
+        // creation d'un comportement qui enverra le texte 'tictac' toutes les secondes
+        TickerBehaviour compTicTac = new TickerBehaviour(AgentPosteur.this, 1000) {
+            @Override
+            protected void onTick() {
+                var msg = new ACLMessage(ACLMessage.INFORM);
+                //ajout d'un identifiant au msg
+                msg.setConversationId("CLOCK");
+                msg.addReceiver(new AID("agentPiege", AID.ISLOCALNAME));
+                msg.setContent("tictac");
+                myAgent.send(msg);
+            }};
+        // ajout d'un comportement qui enverra le texte 'tictac' toutes les secondes  dans 1 secondes
+        addBehaviour(new WakerBehaviour(this, 1000) {
+        protected void onWake() { myAgent.addBehaviour(compTicTac); } });
+        // ajout d'un comportement qui enverra le texte 'boom'   dans 10 secondes
+        addBehaviour(new WakerBehaviour(this, 10000) {
+            protected void onWake() {
+                    var msg = new ACLMessage(ACLMessage.INFORM);
+                    msg.setConversationId("BOOM");
+                    msg.addReceiver(new AID("agentPiege", AID.ISLOCALNAME));
+                    msg.setContent("b o o m ! ! !");
+                    myAgent.removeBehaviour(compTicTac);
+                    myAgent.send(msg);
+            }});
+
+    }
+
+}
