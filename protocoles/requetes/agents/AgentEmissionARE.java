8fde7f33c24ad59f83611b083fbec0a64cae5726
EmmanuelADAM <emmanueladam@free.fr>
2022-11-19 16:58:51 +0100

    Update AgentEmissionARE.java

--- protocoles/requetes/agents/AgentEmissionARE.java
+++ protocoles/requetes/agents/AgentEmissionARE.java
@@ -15,65 +15,70 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * classe d'un agent qui soumet une requête de somme à un autre agent et gère l'échange par le protocole AchieveRE
+ * Class for an agent that send a request to other agents about a sum of integers to calculate.
+ * The other agents can refuse or agree to calculate and then send their result.
+ * This agent use an AchieveRE protocol to manage the messages.
  *
  * @author eadam
  */
 public class AgentEmissionARE extends AgentWindowed {
 
     /**
-     * ajout du suivi de protocole AchieveRE
+     * agent set-up
      */
     protected void setup() {
         window = new SimpleWindow4Agent(getAID().getName(), this);
-        window.println("Hello! Agent  " + getLocalName() + " is ready, my address is " + this.getAID().getName());
+        window.println("Hello! I'm ready, my address is " + this.getAID().getName());
+        window.println("Click to the button to launch requests");
+        window.println("-".repeat(40));
         window.setButtonActivated(true);
         window.setBackgroundTextColor(Color.CYAN);
     }
 
     /**
-     * add a AchieveREInitiator protocol to send a request
+     * add a AchieveREInitiator protocol to send a request about an addition and wait manage the responses
      */
     private void createRequest(String id, String computation) {
         ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
         msg.setConversationId(id);
         msg.setContent(computation);
 
-        var adresses = AgentServicesTools.searchAgents(this, "calcul", "somme");
+        var adresses = AgentServicesTools.searchAgents(this, "calculation", "addition");
         msg.addReceivers(adresses);
-        println("agents calculateurs trouves : " + Arrays.stream(adresses).map(AID::getLocalName).toList().toString());
+        println("Calculator agents found: " + Arrays.stream(adresses).map(AID::getLocalName).toList());
 
         println("_".repeat(40));
-        println("j'envoie une requete sur " + msg.getContent());
+        println("I send a request for " + msg.getContent());
         println("_".repeat(40));
+        //AchieveRE protocol, the msg is sent at its activation
         AchieveREInitiator init = new AchieveREInitiator(this, msg) {
-            //fonction lancée dès accord
+            //function triggered when receiving an AGREE message (sender is agree to send a result)
             @Override
             protected void handleAgree(ACLMessage agree) {
-                window.println("recu un accord de " + agree.getSender().getLocalName());
+                window.println("Received an agree msg from " + agree.getSender().getLocalName());
             }
 
-            //fonction lancée dès accord
+            //function triggered when receiving an REFUSE message (sender refuses to send a result)
             @Override
             protected void handleRefuse(ACLMessage refuse) {
-                window.println("recu un refus de " + refuse.getSender().getLocalName());
+                window.println("Received a refuse msg from " + refuse.getSender().getLocalName());
             }
 
-            //fonction lancée dès reception information
+            //function triggered when receiving an INFORM message (sender gives its result)
             @Override
             protected void handleInform(ACLMessage inform) {
-                window.println("recu  de " + inform.getSender().getLocalName() +
-                        ", ce resultat " + inform.getContent());
+                window.println("Received from " + inform.getSender().getLocalName() + ", this result: " + inform.getContent());
             }
 
 
-            //fonction lancée dès que toutes les réponses ont été reçues
+            //function triggered when all receivers have sent a response (agree&inform or refuse)
+            // responses = list of received messages (inform & refuse)
             @Override
             protected void handleAllResultNotifications(List<ACLMessage> responses) {
                 println("~".repeat(40));
-                StringBuilder sb = new StringBuilder("c'est bon, j'ai toutes les reponses. Pour rappel : \n");
+                StringBuilder sb = new StringBuilder("Ok, I've all the responses. To resume: \n");
                 for (ACLMessage msg : responses) {
-                    sb.append("\t-de ").append(msg.getSender().getLocalName()).append(" : ").append(msg.getContent()).append("\n");
+                    sb.append("\t-from ").append(msg.getSender().getLocalName()).append(" : ").append(msg.getContent()).append("\n");
                 }
                 println(sb.toString());
                 println("(o)".repeat(20));
@@ -84,6 +89,9 @@ public class AgentEmissionARE extends AgentWindowed {
     }
 
 
+    /**reaction to an event sent by the gui:
+     * create a request with the shape sum x,y,z,...
+     * and ask to add an achieveReInitior behaviour */
     protected void onGuiEvent(GuiEvent arg0) {
         Random r = new Random();
         int nb = r.nextInt(3, 7);
